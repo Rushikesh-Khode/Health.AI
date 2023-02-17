18d02d3152502414f06c46f657a564413dafde73
rushikesh <rushikesh.khode@noovosoft.com>
2023-02-17 17:38:38 +0530

    Add notumor class to ml model

--- brain_tumor_classification/ml_model.py
+++ brain_tumor_classification/ml_model.py
@@ -11,21 +11,9 @@ brain_tumor_classification_model = tf.keras.models.load_model(
     os.path.join("saved_ml_models", "brain_tumor_classification"))
 
 
-def convert_image_to_gray(image):
-    if image.mode == "RGB":
-        return tf.image.rgb_to_grayscale(image)
-    if image.mode == "RGBA":
-        image = np.array(image, dtype=float32)
-        return tf.image.decode_png(image)
-    raise Exception("Unsupported Color Format")
-
-
 def preprocess_image(image):
     image = Image.open(io.BytesIO(base64.decodebytes(bytes(image, "utf-8"))))
-    image = convert_image_to_gray(image)
     image = tf.image.resize(image, [256, 256])
-    normalize = tf.keras.layers.Rescaling(1. / 255)
-    image = normalize(image)
     image = tf.expand_dims(image, axis=0)
     return image
 
@@ -33,7 +21,8 @@ def preprocess_image(image):
 def predict(image):
     image = preprocess_image(image)
     predication = brain_tumor_classification_model.predict(image, verbose=0)[0]
-    return {"meningioma": predication[0], "glioma": predication[1], "pituitary tumor": predication[2]}
+    return {'glioma': predication[0], 'meningioma': predication[1], 'notumor': predication[2],
+            'pituitary': predication[3]}
 
 
 # dev only
